42879a5fb662e22d2c91bd7c2c54b6869b65351a
Ayush Chauhan <chauhan0007ayush@gmail.com>
2026-08-27 12:01:26 +0530

    third commit

---
+++ webapp/controller/HelloDialog.js
@@ -0,0 +1,42 @@
+sap.ui.define(
+  ["sap/ui/base/ManagedObject", "sap/ui/core/Fragment"],
+  function (ManagedObject, Fragment) {
+    "use strict";
+
+    return ManagedObject.extend("sap.ui.walkthrough.controller.HelloDialog", {
+      constructor: function (oView) {
+        this._oView = oView;
+      },
+
+      exit: function () {
+        delete this._oView;
+      },
+
+      open: function () {
+        var oView = this._oView;
+
+        // create the dialog lazily.
+        if (!oView.byId("helloDialog")) {
+          var oFragmentController = {
+            onCloseDialog: function () {
+              oView.byId("helloDialog").close();
+            },
+          };
+
+          // Load asynchronous XML Fragment.
+          Fragment.load({
+            id: oView.getId(),
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+            controller: oFragmentController,
+          }).then(function (oDialog) {
+            // connect dialog to the root view of the Component (models, lifecycle).
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
+        } else {
+          oView.byId("helloDialog").open();
+        }
+      },
+    });
+  },
+);
